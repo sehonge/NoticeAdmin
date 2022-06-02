ec29431bc07a078e7ba84c17a411c1c19d43fc1a
sehonge <sehong1602@naver.com>
2022-06-02 17:03:39 +0900

    checkUserId, checkPasswordLength, checkPasswordCheck 생성

--- src/utils/validateUser.js
+++ src/utils/validateUser.js
@@ -1,3 +1,5 @@
+import {get} from "./fetchutil";
+
 const validateUser = (id, password) => {
     return validate(password, 8, 20) && validate(id, 6, 15);
 }
@@ -14,6 +16,51 @@ const validate = (target, minLen, maxLen) => {
     }
 }
 
+const duplicateCheck = async (parameter) => {
+    let response = await get("/api/user/register", parameter);
+    await console.log(response);
+    if (response.status === 200) {
+        return false;
+    } else if (response.status === 400) {
+        console.log(1);
+        return true;
+    } else if (response.status >= 500) {
+        alert("일시적인 서버 오류가 발생했습니다. 잠시 후 시도해 주십시오.");
+        return true;
+    }
+}
+
+const checkUserId = (userId) => {
+    if (userId.length < 6 || userId.length > 15) {
+        return "아이디의 길이는 6자 이상이고 15자 이하여야 합니다.";
+    } else {
+        const parameter = {
+            userId: userId
+        }
+        let response = duplicateCheck(parameter);
+        console.log(response);
+        // if (response === true) {
+        //     return "이미 회원가입된 아이디 입니다.";
+        // } else {
+        //     return "";
+        // }
+    }
+}
+
+const checkPasswordLength = (password) => {
+    if (password.length < 8 || password.length > 20) {
+        return "비밀번호의 길이는 8자 이상이고 20자 이하여야 합니다.";
+    } else {
+        return " ";
+    }
+}
+
+const checkPasswordCheck = (password, passwordCheck) => {
+    if (password !== passwordCheck) {
+        return "비밀번호가 일치하지 않습니다."
+    }
+}
+
 export {
-    validateUser
+    validateUser, checkUserId, checkPasswordCheck, checkPasswordLength
 }
